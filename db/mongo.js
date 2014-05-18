99747addfc22a545d09cf98377cad00c90d1d560
parisminton <parisminton@da.ydrea.ms>
2014-05-18 05:47:53 -0400

    mongo handling gets its own file.

---
+++ db/mongo.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose'),
+    db,
+    trainingBoutSchema,
+    TrainingBout,
+    bout,
+    saveBout;
+
+mongoose.connect('mongodb://localhost/butkus');
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('You, sir, are connected to mongo.');
+});
+
+trainingBoutSchema = mongoose.Schema({
+  current_weight : Number,
+  date : Date,
+  day : String,
+  time : Number,
+  immediate_protein : Boolean
+});
+
+// create any schema methods before this instantiation
+TrainingBout = mongoose.model('TrainingBout', trainingBoutSchema);
+
+saveBout = function (data) {
+  bout = new TrainingBout ({
+    current_weight : data.weight,
+    date : new Date(),
+    day : data.day,
+    time : new Date().getTime(),
+    immediate_protein : true
+  });
+
+  bout.save(function (err, bout) {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      console.log('This bout has been saved.');
+    }
+  });
+} // end saveBout
+
+// saveBout( { weight : 176, day : 'Boxing Day' } );
