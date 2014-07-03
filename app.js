72143c15ede750a37ad5ab9bd532263703dde81b
parisminton <parisminton@da.ydrea.ms>
2014-07-03 02:05:31 -0400

    Route using my CRUD functions.

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/exercise', routes.exercise);
+app.get('/', routes.index.read);
+app.get('/log', routes.log.read);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
